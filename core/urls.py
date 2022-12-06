2ec4cd263de762104658b07c6eaeeaf2677b5794
ФÐArtem <goldaev51@gmail.com>
2022-12-06 19:39:15 +0200

    create registration logic

--- core/urls.py
+++ core/urls.py
@@ -21,7 +21,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('accounts/', include('django.contrib.auth.urls'))
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('authentication.urls'))
 ]
 
 if settings.DEBUG:
